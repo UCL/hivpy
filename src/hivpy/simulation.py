f903ce4745a720d1e06e1dc4787874400e8a5528
Michael A. McLeod <michael.mcleod.13@ucl.ac.uk>
2022-01-31 10:44:35 +0000

    Add simulation output for HIV prevalence by age and sex.

--- src/hivpy/simulation.py
+++ src/hivpy/simulation.py
@@ -1,10 +1,88 @@
 import logging
+import operator
+import string
+from asyncio.windows_events import NULL
+from datetime import datetime
 
+import numpy as np
 import pandas as pd
 
 from .config import SimulationConfig
+from .demographics import SexType
 from .exceptions import SimulationException
 from .population import Population
+from .sexual_behaviour import selector
+
+
+class SimulationOutput:
+    file_path: string
+    output_stats: dict
+
+    def _init_output_field(self, key, default):
+        self.output_stats[key] = np.array([default]*self.num_steps)
+
+    def _init_output_fields(self, key_default_pairs):
+        for (k, d) in key_default_pairs:
+            self._init_output_field(k, d)
+
+    def __init__(self, simulation_config: SimulationConfig):
+        self.num_steps = int((simulation_config.stop_date -
+                             simulation_config.start_date) / simulation_config.time_step)
+        self.output_stats = {"Date": np.array([simulation_config.start_date]*self.num_steps)}
+        self._init_output_fields([("HIV prevalence (tot)", 0.0),
+                                  ("HIV prevalence (male)", 0.0),
+                                  ("HIV prevalence (female)", 0.0),
+                                  ("HIV infections (tot)", 0),
+                                  ("Population (over 15)", 0)])
+        self.file_path = "output/simulation_output." + \
+            str(datetime.now().strftime("%Y%m%d-%H%M%S")) + ".csv"
+        self.age_min = 15
+        self.age_max = 100
+        self.age_step = 10
+        self.step = 0
+
+    def _update_date(self, date):
+        self.output_stats["Date"][self.step] = date
+
+    def _ratio(self, subpop, pop):
+        if sum(pop) != 0:
+            return sum(subpop)/sum(pop)
+        else:
+            return NULL
+
+    def _update_HIV_prevalence(self, pop_data):
+        # Update total HIV cases and population
+        over_15_idx = selector(pop_data, age=(operator.gt, 15))
+        HIV_pos_idx = selector(pop_data, HIV_status=(operator.eq, True))
+        self.output_stats["HIV prevalence (tot)"][self.step] = self._ratio(HIV_pos_idx, over_15_idx)
+        self.output_stats["HIV infections (tot)"][self.step] = sum(HIV_pos_idx)
+        self.output_stats["Population (over 15)"][self.step] = sum(over_15_idx)
+
+        # Update HIV prevalence by sex
+        men_idx = selector(pop_data, sex=(operator.eq, SexType.Male))
+        self.output_stats["HIV prevalence (male)"][self.step] = (
+            self._ratio(HIV_pos_idx & men_idx, men_idx & over_15_idx))
+        women_idx = selector(pop_data, sex=(operator.eq, SexType.Female))
+        self.output_stats["HIV prevalence (female)"][self.step] = self._ratio(
+            HIV_pos_idx & women_idx, women_idx & over_15_idx)
+
+        # Update HIV prevalence by age
+        for age_bound in range(self.age_min, self.age_max, self.age_step):
+            key = "HIV prevalence (" + str(age_bound) + "-" + str(age_bound+(self.age_step-1)) + ")"
+            age_idx = (pop_data["age"] >= age_bound) & (
+                pop_data["age"] < (age_bound + self.age_step))
+            if(key not in self.output_stats.keys()):
+                self._init_output_field(key, 0.0)
+            self.output_stats[key][self.step] = self._ratio(HIV_pos_idx & age_idx, age_idx)
+
+    def update_summary_stats(self, date, pop_data):
+        self._update_date(date)
+        self._update_HIV_prevalence(pop_data)
+        self.step += 1
+
+    def write_output(self):
+        df = pd.DataFrame(self.output_stats)
+        df.to_csv(self.file_path, mode='w')
 
 
 class SimulationHandler:
@@ -17,6 +95,7 @@ class SimulationHandler:
         self.simulation_config = simulation_config
         self.results = None
         self._initialise_population()
+        self.output = SimulationOutput(self.simulation_config)
 
     def _initialise_population(self):
         self.population = Population(self.simulation_config.population_size,
@@ -42,12 +121,14 @@ class SimulationHandler:
             # Advance the population
             self.population = self.population.evolve(time_step)
             date = date + time_step
+            self.output.update_summary_stats(date, self.population.data)
             # Record the values of the tracked attributes
             if tracked_attrs:  # we need this because we can't set an empty row
                 results.loc[date] = {
                     attr: self.population.get(attr) for attr in tracked_attrs
                 }
         logging.info("finished")
+        self.output.write_output()
         self.results = results
 
 
