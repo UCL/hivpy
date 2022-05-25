57fdf99f5f705b3a069cfe3f2970fe8a4cf62b79
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2022-05-25 12:36:01 +0100

    Start testing the default demographics data

---
+++ src/tests/test_demographics_data.py
@@ -0,0 +1,20 @@
+import pytest
+
+from hivpy.common import SexType
+from hivpy.demographics_data import DemographicsData
+
+
+@pytest.fixture(scope="module")
+def default_data():
+    path = "data/demographics.yaml"
+    return DemographicsData(path)
+
+
+def test_death_rate_ages(default_data):
+    """Check that the default death rate specification is consistent."""
+    male_death_rates = default_data.death_rates[SexType.Male]
+    female_death_rates = default_data.death_rates[SexType.Female]
+    age_limits = default_data.death_age_limits
+    assert len(male_death_rates) == len(female_death_rates)
+    # We only record the lower limit and forego the first group (assumed death rate 0)
+    assert len(age_limits) == len(male_death_rates) - 1
