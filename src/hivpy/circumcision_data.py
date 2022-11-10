d35bc189f718344825746e1ce66f92867de85233
unknown <em.dubrovska@gmail.com>
2022-11-10 13:45:09 +0000

    Adding additional columns and data for VMMC.

--- src/hivpy/circumcision_data.py
+++ src/hivpy/circumcision_data.py
@@ -7,8 +7,12 @@ from .common import DiscreteChoice
 
 
 class CircumcisionData:
-    """Class to hold and interpret circumcision data loaded from the yaml file"""
+    """
+    Class to hold and interpret circumcision data loaded from the yaml file.
+    """
 
+    # TODO: This is ripped directly from sex_behaviour_data.py,
+    # we should make a new data reader module to store functions like this.
     def _get_discrete_dist(self, *keys):
         dist_data = self.data
         for k in keys:
@@ -22,6 +26,9 @@ class CircumcisionData:
         with open(filename, 'r') as file:
             self.data = yaml.safe_load(file)
         try:
+            self.mc_int = self.data["mc_int"]
+            self.test_link_circ = self.data["test_link_circ"]
+            self.test_link_circ_prob = self.data["test_link_circ_prob"]
             self.circ_inc_rate = self._get_discrete_dist("circ_inc_rate")
             self.rel_incr_circ_post_2013 = self._get_discrete_dist("rel_incr_circ_post_2013")
             self.circ_inc_15_19 = self._get_discrete_dist("circ_inc_15_19")
