225ff1f15076dc11077d0663dd36160235bd477f
unknown <em.dubrovska@gmail.com>
2023-01-11 13:37:14 +0000

    Added childbirth test.

--- src/tests/test_pregnancy.py
+++ src/tests/test_pregnancy.py
@@ -38,9 +38,9 @@ def test_new_preg():
 
         # age restrictions on those who can get pregnant
         if age < 15:
-            assert sum((pop.data[col.AGE] < 15) & pop.data[col.PREGNANCY_DATE].notnull()) == 0
+            assert sum((pop.data[col.AGE] < 15) & pop.data[col.PREGNANT]) == 0
         elif age >= 55:
-            assert sum((pop.data[col.AGE] >= 55) & pop.data[col.PREGNANCY_DATE].notnull()) == 0
+            assert sum((pop.data[col.AGE] >= 55) & pop.data[col.PREGNANT]) == 0
 
         else:
             # get stats
@@ -48,19 +48,19 @@ def test_new_preg():
                                    & (~pop.data[col.LOW_FERTILITY])
                                    & ((pop.data[col.NUM_PARTNERS] > 0)
                                       | pop.data[col.LONG_TERM_PARTNER]))
-            no_pregnant = sum(pop.data[col.PREGNANCY_DATE].notnull())
+            no_pregnant = sum(pop.data[col.PREGNANT])
             prob_preg = pop.pregnancy.prob_pregnancy_base * pop.pregnancy.fold_preg[test_ages.index(age)-1]
             mean = no_active_female * prob_preg
             stdev = sqrt(mean * (1 - prob_preg))
             # check pregnancy value is within 3 standard deviations
             assert mean - 3 * stdev <= no_pregnant <= mean + 3 * stdev
             # low fertility individuals don't become pregnant
-            assert sum(pop.data[col.LOW_FERTILITY] & pop.data[col.PREGNANCY_DATE].notnull()) == 0
+            assert sum(pop.data[col.LOW_FERTILITY] & pop.data[col.PREGNANT]) == 0
 
 
 def test_childbirth():
 
-    N = 100000
+    N = 100
     start_date = date(1990, 1, 1)
     time_step = timedelta(days=90)
 
@@ -68,8 +68,10 @@ def test_childbirth():
     pop = Population(size=N, start_date=start_date)
     pop.data[col.SEX] = SexType.Female
     pop.data[col.AGE] = 18
+    pop.data[col.LOW_FERTILITY] = False
     pop.data[col.NUM_PARTNERS] = 1
-    pop.pregnancy.init_fertility(pop)
+    # guaranteed pregnancy
+    pop.pregnancy.prob_pregnancy_base = 1
 
     # evolve population
     # TODO: may need to account for including time step period when updating pregnancy
@@ -78,8 +80,19 @@ def test_childbirth():
         pop.pregnancy.update_pregnancy(pop)
         pop.date += time_step
 
-    pregnant_at_start = pop.data.index[pop.data[col.PREGNANCY_DATE] == start_date]
     # final advancement into childbirth
     pop.pregnancy.update_pregnancy(pop)
-    # check that all who got pregnant during the start date are now no longer pregnant
-    assert pop.data.loc[pregnant_at_start, col.PREGNANCY_DATE].isnull().all()
+    pop.date += time_step
+    # check that nobody is pregnant anymore
+    assert ~pop.data[col.PREGNANT].all()
+
+    # test the pregnancy pause period
+    pop.pregnancy.update_pregnancy(pop)
+    pop.date += time_step
+    # check that there are still no pregnancies
+    assert ~pop.data[col.PREGNANT].all()
+
+    pop.pregnancy.update_pregnancy(pop)
+    pop.date += time_step
+    # check that everyone is pregnant again
+    assert pop.data[col.PREGNANT].all()
