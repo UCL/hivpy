9dbea156d39b5c7263c69a2ab983c76c0e39993b
Michael A. McLeod <michael.mcleod.13@ucl.ac.uk>
2024-05-15 14:11:15 +0100

    Remove partner ratio output test as it no longer makes sense

--- src/tests/test_output.py
+++ src/tests/test_output.py
@@ -64,25 +64,3 @@ def test_HIV_incidence():
         age_group = int(age_bound/10)-1
         key = f"HIV incidence ({age_bound}-{age_bound+(age_step-1)}, female)"
         assert isclose(out.output_stats[key], 1-age_group*0.25)
-
-
-def test_partner_sex_balance():
-
-    # build population
-    N = 100
-    pop = Population(size=N, start_date=date(1990, 1, 1))
-    pop.data.loc[:int(N/2)-1, col.SEX] = SexType.Female
-    pop.data.loc[int(N/2):, col.SEX] = SexType.Male
-    pop.data[col.AGE] = 25
-    pop.data[col.NUM_PARTNERS] = 2
-    pop.data[col.STP_AGE_GROUPS] = [[1, 2]]*N
-
-    out = SimulationOutput(date(1990, 1, 1), date(1990, 3, 1), timedelta(days=90))
-    out._update_partner_sex_balance(pop)
-
-    # check overall sex balance is equal
-    assert isclose(out.output_stats["Partner sex balance (male)"], 0)
-    assert isclose(out.output_stats["Partner sex balance (female)"], 0)
-    # check age-specific ratio is 2
-    assert isclose(out.output_stats["Partner sex balance (25-34, male)"], log(2, 10))
-    assert isclose(out.output_stats["Partner sex balance (25-34, female)"], log(2, 10))
