9a821b9c033012f717e3528cddfdd2257a528fcc
unknown <em.dubrovska@gmail.com>
2025-01-20 18:32:17 +0000

    Added CD4 calculation test.

--- src/tests/test_resistance_mutations.py
+++ src/tests/test_resistance_mutations.py
@@ -1,6 +1,6 @@
-from math import isclose
+from math import isclose, sqrt
 
-from hivpy.common import date, timedelta
+from hivpy.common import SexType, date, timedelta
 from hivpy.population import Population
 
 
@@ -55,6 +55,65 @@ def test_calc_viral_load():
     assert (res.min_vl_on_art - res.vl_stdev_on_art * 3 <=
             res.calc_viral_load(3, timedelta(months=6), 0.8, 0.8, max_viral_load)
             <= res.min_vl_on_art + res.vl_stdev_on_art * 3)
+
+def test_calc_cd4_delta():
+    pop = Population(size=1, start_date=date(2000, 1, 1))
+    res = pop.resistance
+    res.hindered_cd4_recovery = -3
+
+    # check basic case at age 20
+    # 6 + 0.1 * -18 = 4.2
+    # 50 + 4.2 = 54.2
+    cd4, delta = res.calc_cd4_delta(20, SexType.Male, 0, timedelta(months=0), 0, 0,
+                                    False, False, False, False, False, False, 50, 0.1, 100, False, False)
+    assert isclose(cd4, 54.2)
+    assert isclose(delta, 4.2)
+    # 6 + 0.1 * 30 = 9
+    # 50 + 9 = 59
+    cd4, delta = res.calc_cd4_delta(20, SexType.Male, 3, timedelta(months=6), 0.8, 0.8,
+                                    False, False, False, False, False, False, 50, 0.1, 100, False, False)
+    assert isclose(cd4, 59)
+    assert isclose(delta, 9)
+
+    # check basic case with female recovery factor
+    # 6 + 2 + 0.1 * 30 = 11
+    # 50 + 11 = 61
+    cd4, delta = res.calc_cd4_delta(20, SexType.Female, 3, timedelta(months=6), 0.8, 0.8,
+                                    False, False, False, False, False, False, 50, 0.1, 100, False, False)
+    assert isclose(cd4, 61)
+    assert isclose(delta, 11)
+
+    # check hindered cd4 recovery
+    # 3 + 0.1 * -18 = 1.2
+    # 50 + 1.2 = 51.2
+    cd4, delta = res.calc_cd4_delta(20, SexType.Male, 0, timedelta(months=0), 0, 0,
+                                    True, False, False, False, False, False, 50, 0.1, 100, False, False)
+    assert isclose(cd4, 51.2)
+    assert isclose(delta, 1.2)
+
+    # check improved cd4 recovery with pi recovery factor
+    # 9 + 0.1 * 30 = 12
+    # 50 + 12 = 62
+    cd4, delta = res.calc_cd4_delta(20, SexType.Male, 3, timedelta(months=6), 0.8, 0.8,
+                                    False, False, False, False, False, True, 50, 0.1, 100, False, False)
+    assert isclose(cd4, 62)
+    assert isclose(delta, 12)
+
+    # check adjustments on ARV
+    # 6 + 0.2 * 6 = 12 >> 12 * 0.85 = 10.2
+    # 150 + 12 = 162 >> sqrt(162) + cd4_stdev_on_art * rng.normal() ** 2
+    cd4, delta = res.calc_cd4_delta(20, SexType.Male, 3, timedelta(months=6), 0.8, 0.8,
+                                    False, False, False, False, False, False, 150, 0.2, 200, True, False)
+    assert sqrt(162) - res.cd4_stdev_on_art * 3 <= cd4 <= sqrt(162) + res.cd4_stdev_on_art * 3
+    assert isclose(delta, 10.2)
+
+    # check max cd4 cap on ARV
+    # 6 + 0.2 * 6 = 12 >> 12 * 0.7 = 8.4
+    # 100 + rng.normal() * 50
+    cd4, delta = res.calc_cd4_delta(20, SexType.Male, 3, timedelta(months=6), 0.8, 0.8,
+                                    False, False, False, False, False, False, 10000, 0.2, 100, False, True)
+    assert 100 - 50 * 3 <= cd4 <= 100 + 50 * 3
+    assert isclose(delta, 8.4)
 
 def test_calc_prob_new_mutation():
     pop = Population(size=1, start_date=date(2000, 1, 1))
