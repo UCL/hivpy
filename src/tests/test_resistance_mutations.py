42ce0ef6484edba090b388f37042bb5b592d9405
unknown <em.dubrovska@gmail.com>
2025-02-19 17:04:12 +0000

    Added PR54 and PR76 mutation tests.

--- src/tests/test_resistance_mutations.py
+++ src/tests/test_resistance_mutations.py
@@ -681,6 +681,58 @@ def test_pr50vm():
     assert mean - 3 * stdev <= mutated <= mean + 3 * stdev
 
 
+def test_pr54m():
+    N = 1000
+    pop = Population(size=N, start_date=date(2020, 1, 1))
+    pop.set_present_variable(col.HIV_STATUS, True)
+    pop.set_present_variable(col.NUM_ACTIVE_DRUGS, 0)
+    pop.data[pop.get_correct_column(col.CONT_ON_ART, dt=1)] = timedelta(months=0)
+    pop.set_present_variable(col.ART_ADHERENCE, 0.5)
+    # the entire population has a chance to gain mutations
+    pop.set_present_variable(col.VIRAL_LOAD, 10)
+    pop.set_present_variable(col.ON_LPR, True)
+    pop.set_present_variable(col.ON_DAR, True)
+
+    res = pop.resistance
+    res.mutation_risk_change = 0.5
+    res.active_drug_indices, res.cont_on_art_tm1_indices, \
+        res.adherence_indices, res.adherence_tm1_indices = res.get_all_matrix_indices(pop, pop.data.index)
+    pop.set_present_variable(col.RESISTANCE_INDEX, range(len(pop.data.index)), pop.data.index)
+    res.update_new_mutations_arising_art(pop, pop.data.index)
+
+    mutated = len(pop.get_sub_pop([(col.PR54_MUTATION, op.eq, True)]))
+    mean = N * 0.03
+    stdev = sqrt(mean * (1 - 0.03))
+    # expecting ~3% of the population to gain pr54m
+    assert mean - 3 * stdev <= mutated <= mean + 3 * stdev
+
+
+def test_pr76m():
+    N = 1000
+    pop = Population(size=N, start_date=date(2020, 1, 1))
+    pop.set_present_variable(col.HIV_STATUS, True)
+    pop.set_present_variable(col.NUM_ACTIVE_DRUGS, 0)
+    pop.data[pop.get_correct_column(col.CONT_ON_ART, dt=1)] = timedelta(months=0)
+    pop.set_present_variable(col.ART_ADHERENCE, 0.5)
+    # the entire population has a chance to gain mutations
+    pop.set_present_variable(col.VIRAL_LOAD, 10)
+    pop.set_present_variable(col.ON_LPR, True)
+    pop.set_present_variable(col.ON_DAR, True)
+
+    res = pop.resistance
+    res.mutation_risk_change = 0.5
+    res.active_drug_indices, res.cont_on_art_tm1_indices, \
+        res.adherence_indices, res.adherence_tm1_indices = res.get_all_matrix_indices(pop, pop.data.index)
+    pop.set_present_variable(col.RESISTANCE_INDEX, range(len(pop.data.index)), pop.data.index)
+    res.update_new_mutations_arising_art(pop, pop.data.index)
+
+    mutated = len(pop.get_sub_pop([(col.PR76_MUTATION, op.eq, True)]))
+    mean = N * 0.03
+    stdev = sqrt(mean * (1 - 0.03))
+    # expecting ~3% of the population to gain pr76m
+    assert mean - 3 * stdev <= mutated <= mean + 3 * stdev
+
+
 def test_update_resistance():
     N = 100
     time_step = timedelta(months=1)
